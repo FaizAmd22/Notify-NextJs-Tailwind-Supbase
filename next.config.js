b3645859f34f939819d04e58c8f619d37297d6c6
Faizhal Ahmad <ahmadfaizhal8@gmail.com>
2023-11-06 21:45:27 +0700

    Menambahkan semua file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ["maswvxeautrvefnhovcw.supabase.co"]
+    },
+  
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["maswvxeautrvefnhovcw.supabase.co"]
+//   }
+// }
+
+// module.exports = nextConfig
